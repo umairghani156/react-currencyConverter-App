f7a6e20905976ad5bcd5444fbc9db3a00fc8341a
umairghani156 <umairghani156@gmail.com>
2024-02-24 15:32:46 +0500

    currencyConverterApp

---
+++ src/components/Answer.jsx
@@ -0,0 +1,13 @@
+const Answer = () => {
+    return(
+        <div style={{width:"100%", backgroundColor:"#fff", padding:"3px", border:"2px solid #afafaf", borderRadius:"10px"}}>
+            <h1 style={{fontSize:"22px", fontWeight:"600"}}>Answer</h1>
+            <div style={{display:"flex", flexDirection:"column", gap:"15px", alignItems:"center"}}>
+                <p>Hello</p>
+                <p>Hello</p>
+                <p>Hello</p>
+            </div>
+        </div>
+    )
+}
+export default Answer;
